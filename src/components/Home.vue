fe1ff5014f0324e2944fbfacde82730a06318310
jimrex <ijimrex@gmail.com>
2017-05-16 16:44:25 +0800

    Home mid

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,14 +3,25 @@
 
     <HeaderUp></HeaderUp>
     <div id="searchBody">
-      <div class="row">
+      <div id="videobg">
         <video autoplay muted loop style="width:100%;z-index: -999;position:absolute;" poster="/static/Cheer-Up/Snapshots/Cheer-Up.jpg">
           <source src="/static/Cheer-Up/MP4/Cheer-Up.mp4">
         </video>
-        <div class="col-xs-12 col-sm-6">.col-xs-12 .col-sm-3</div>
-        <div class="col-xs-12 col-sm-6">.col-xs-12 .col-sm-3</div>
-
       </div>
+      <div class="container">
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search for...">
+            <span class="input-group-btn">
+        <button class="btn btn-default" type="button">Go!</button>
+      </span>
+          </div><!-- /input-group -->
+        </div><!-- /.col-lg-6 -->
+      </div>
+      </div>
+
+
 
 
     </div>
@@ -35,7 +46,23 @@
 }
 </script>
 <style>
+  @media all and (max-width: 765px) {
+    #videobg{
+      display: none;
+    }
+    #searchBody{
+      margin-top: 50px;
+      background: darkslateblue;
+      min-height: 400px;
+
+    }
+
+  }
   #searchBody{
     margin-top: 50px;
+
+  }
+  #videobg{
+
   }
 </style>
