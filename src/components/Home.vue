63ad1445ea44ffd11d922a8959ef91c6e21c85f8
jimrex <ijimrex@gmail.com>
2017-05-18 11:57:28 +0800

    home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,29 +8,38 @@
           <source src="/static/Cheer-Up/MP4/Cheer-Up.mp4">
         </video>
       </div>
-      <div class="container">
-      <div class="row">
-        <div class="col-lg-6">
+      <div class="container" >
+      <div class="row" >
+        <div class="col-lg-12 text-center">
           <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search for...">
+            <div class="input-group-btn">
+              <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">搜索类型<span class="caret"></span></button>
+              <ul class="dropdown-menu">
+                <li><a href="#">歌名</a></li>
+                <li><a href="#">歌手</a></li>
+                <li><a href="#">专辑</a></li>
+
+              </ul>
+            </div><!-- /btn-group -->
+            <input type="text" class="form-control" aria-label="...">
             <span class="input-group-btn">
-        <button class="btn btn-default" type="button">Go!</button>
+        <button class="btn btn-default" type="button">搜索</button>
       </span>
           </div><!-- /input-group -->
+
+
         </div><!-- /.col-lg-6 -->
       </div>
+
       </div>
+    </div>
 
-
-
+    <div id="rank">
 
     </div>
+
   </div>
 </template>
-
-
-
-
 
 
 <script>
@@ -60,9 +69,17 @@
   }
   #searchBody{
     margin-top: 50px;
+    height: 100%;
 
   }
   #videobg{
+
+  }
+  .toMiddle{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 
   }
 </style>
